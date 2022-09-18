2502218d7ac62297ec6918d564b7c47c222796ca
Pons070 <112777729+Pons070@users.noreply.github.com>
2022-09-18 11:57:39 +0530

    Add files via upload

---
+++ AxisBank.java
@@ -0,0 +1,10 @@
+package week3.day1;
+
+public class AxisBank extends BankInfo{
+	
+	public void deposit() {
+		System.out.println("Your overall deposited amount for this month is Ruppes 50Lakhs");
+	}
+
+	
+}
---
+++ BankInfo.java
@@ -0,0 +1,18 @@
+package week3.day1;
+
+public class BankInfo {
+
+	public void saving() {
+		
+		System.out.println("You have Rupees 3 lakhs in your savings account");
+	}
+	
+	public void deposit() {
+		System.out.println("You have deposited Rupees 10 lakhs in your savings account");
+	}
+	
+	public void fixed() {
+		System.out.println("Your FD accounts interest is increased");
+	}
+	
+}
