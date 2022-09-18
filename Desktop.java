b5db186eecdc626b1beae09776d3d8fbbf8564e8
Pons070 <112777729+Pons070@users.noreply.github.com>
2022-09-18 11:23:13 +0530

    Add files via upload

---
+++ Desktop.java
@@ -0,0 +1,18 @@
+package org.system;
+
+public class Desktop extends Computer {
+	
+	public void desktopSize() {
+	
+		System.out.println("Desktop Size: 14inch");
+		
+		Desktop brandDetails = new Desktop();
+		
+		brandDetails.computerModel();
+		brandDetails.desktopSize();
+	
+	}
+	
+	
+
+}
