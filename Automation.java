ae62ca0c149029845b47ba47d2c0b734c6fcb334
Pons070 <112777729+Pons070@users.noreply.github.com>
2022-09-18 12:35:55 +0530

    Add files via upload

---
+++ Automation.java
@@ -0,0 +1,29 @@
+package week3.day1;
+
+public class Automation extends MultipleLangauge{
+
+	public void Java() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void ruby() {
+		// TODO Auto-generated method stub
+		
+	}
+	public void Selenium() {
+	
+	}
+
+	public static void main(String[] args) {
+		Automation Multi=new Automation();
+		
+		Multi.Java();
+		Multi.python();
+		Multi.ruby();
+		Multi.Selenium();
+
+	}
+	
+}
---
+++ MultipleLangauge.java
@@ -0,0 +1,11 @@
+package week3.day1;
+
+public abstract class MultipleLangauge implements Language{
+
+	public void python() {
+		
+	}
+	public abstract void ruby();
+	
+	
+}
